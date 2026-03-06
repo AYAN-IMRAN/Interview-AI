4ad87e71d1a6e9ed646e2722dc572258a0b8493e
AYAN-IMRAN <ayanimranayanahmed@gmail.com>
2026-03-07 01:44:34 +0500

    AI Added

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,10 @@ const connectToDB = require("./src/config/database");
 
 
 
+
 connectToDB()
+
+
 
 
 
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -4,3 +4,20 @@ const { GoogleGenAI }= require('@google/genai')
 const ai = new GoogleGenAI({
     apiKey:process.env.GEMINI_API_KEY
 })
+
+
+async function InvodekedAIGemini() {
+
+    const reponse = await ai.models.generateContent({
+        model:"gemini-2.5-flash-lite",
+        contents:"Hello Gemini ! explain interview in short"
+    })
+
+
+    console.log(reponse.text);
+    
+    
+}
+
+
+module.exports = InvodekedAIGemini
